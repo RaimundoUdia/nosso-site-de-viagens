9ea9d6a1fb37c978f972fabf86efb5b1067b72cd
raimundo <jrsoares@gmail.com>
2020-04-24 18:20:05 -0300

    Criação do Projeto + Conexao com banco de dados

---
+++ nosso-site-de-viagens/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nosso_site_de_viagens.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nosso_site_de_viagens.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options)
+        {
+
+        }
+        public DbSet<Pais> Paises { get; set; }
+    }
+}
---
+++ nosso-site-de-viagens/Models/Pais.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nosso_site_de_viagens.Models
+{
+    public class Pais
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+    }
+}
