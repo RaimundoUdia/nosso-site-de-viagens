5a410e062625f43fbb75eb18efffed7f98da3aec
raimundo <jrsoares@gmail.com>
2020-04-30 10:20:37 -0300

    Correção Private Id set

--- nosso-site-de-viagens/Models/Pais.cs
+++ nosso-site-de-viagens/Models/Pais.cs
@@ -5,7 +5,7 @@ namespace nosso_site_de_viagens.Models
     public class Pais
     {
         [Key]
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
         [Required]
         public string Nome { get; private set; }
